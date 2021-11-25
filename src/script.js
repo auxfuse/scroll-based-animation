c201d453e9f2f622b79c49c6e2138947110a7d84
Anthony O'Brien <aux.fuse@yahoo.com>
2021-11-25 21:20:29 +0000

    Initial commit ❄❄

---
+++ src/script.js
@@ -0,0 +1,153 @@
+import './style.css'
+import * as THREE from 'three'
+import * as dat from 'lil-gui'
+import { MeshToonMaterial, NearestFilter } from 'three'
+
+/**
+ * Debug
+ */
+const gui = new dat.GUI()
+
+const parameters = {
+    materialColor: '#ffeded'
+}
+
+gui
+    .addColor(parameters, 'materialColor')
+    .onChange(() => {
+        material.color.set(parameters.materialColor);
+    })
+
+/**
+ * Base
+ */
+// Canvas
+const canvas = document.querySelector('canvas.webgl')
+
+// Scene
+const scene = new THREE.Scene()
+
+// Texture
+const textureLoader = new THREE.TextureLoader();
+const gradientTexture = textureLoader.load('textures/gradients/5.jpg');
+// gradientTexture.magFilter = THREE.NearestFilter;
+
+// Material
+const material = new THREE.MeshToonMaterial({
+    color: parameters.materialColor,
+    gradientMap: gradientTexture,
+});
+
+
+/**
+ * Objects
+ */
+
+const objectsDistance = 4;
+
+const mesh1 = new THREE.Mesh(
+    new THREE.TorusGeometry(1, 0.4, 16, 60),
+    material
+);
+const mesh2 = new THREE.Mesh(
+    new THREE.ConeGeometry(1, 2, 32),
+    material
+);
+const mesh3 = new THREE.Mesh(
+    new THREE.TorusKnotGeometry(0.8, 0.35, 100, 16),
+    material
+);
+
+mesh1.position.y = - objectsDistance * 0;
+mesh2.position.y = - objectsDistance * 1;
+mesh3.position.y = - objectsDistance * 2;
+
+scene.add(mesh1, mesh2, mesh3);
+
+const sectionMeshes = [
+    mesh1, mesh2, mesh3
+];
+
+// Light
+
+const directionalLight = new THREE.DirectionalLight(
+    0xaae1d8, 1
+);
+directionalLight.position.set(1, 1, 0);
+
+scene.add(directionalLight);
+
+/**
+ * Sizes
+ */
+const sizes = {
+    width: window.innerWidth,
+    height: window.innerHeight
+}
+
+window.addEventListener('resize', () =>
+{
+    // Update sizes
+    sizes.width = window.innerWidth
+    sizes.height = window.innerHeight
+
+    // Update camera
+    camera.aspect = sizes.width / sizes.height
+    camera.updateProjectionMatrix()
+
+    // Update renderer
+    renderer.setSize(sizes.width, sizes.height)
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+})
+
+/**
+ * Camera
+ */
+// Base camera
+const camera = new THREE.PerspectiveCamera(35, sizes.width / sizes.height, 0.1, 100)
+camera.position.z = 6
+scene.add(camera)
+
+/**
+ * Renderer
+ */
+const renderer = new THREE.WebGLRenderer({
+    canvas: canvas,
+    alpha: true
+})
+renderer.setSize(sizes.width, sizes.height)
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+
+// scroll
+let scrollY = window.scrollY;
+
+window.addEventListener('scroll', () => {
+    scrollY = window.scrollY
+})
+
+/**
+ * Animate
+ */
+const clock = new THREE.Clock()
+
+const tick = () =>
+{
+    const elapsedTime = clock.getElapsedTime()
+
+    // animate meshes
+    for(const mesh of sectionMeshes) {
+        mesh.rotation.x = (Math.sin(elapsedTime * 0.35));
+        mesh.rotation.y = (Math.cos(elapsedTime * 0.15));
+    }
+
+    // animate camera
+    camera.position.y = - scrollY / sizes.height * objectsDistance;
+
+    // Render
+    renderer.render(scene, camera)
+
+    // Call tick again on the next frame
+    window.requestAnimationFrame(tick)
+}
+
+tick()
